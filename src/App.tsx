b171a5d0fe8d4604449f59f11ed01486211f33e9
gayathri <gayathriraj.vlr@gmail.com>
2023-04-28 05:09:08 +0530

    moving resume to public

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Resume from "./components/Resume";
-import { Welcome } from "./components/Welcome";
 
 export default function App() {
     return (
--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,7 +5,7 @@ export default function Resume() {
     const [data, setData] = useState<any>(null);
 
     const fetchData = () => {
-        return fetch('/src/assets/data/resume.json').then((res) => res.json()).then((response) => setData(response));
+        return fetch('resume.json').then((res) => res.json()).then((response) => setData(response));
     }
 
     useEffect(() => { fetchData() }, []);
